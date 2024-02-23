43bc26d24e41de292a7f20dccde71dfe82a5a664
ArkanMB <baena.moyaangel@gmail.com>
2024-02-23 19:56:05 +0100

    Update

--- Travo_Rural/Public/Js/Script2.js
+++ Travo_Rural/Public/Js/Script2.js
@@ -1,13 +1,13 @@
-/*=============== SHOW MENU ===============*/
+/*=============== MOSTRAR MENU ===============*/
 const showMenu = (toggleId, navId) => {
   const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId);
 
   toggle.addEventListener("click", () => {
-    // Add show-menu class to nav menu
+    // Añadir la clase show-menu al menu nav
     nav.classList.toggle("show-menu");
 
-    // Add show-icon to show and hide the menu icon
+    // Añadir show-icon para mostrar o ocultar el icono del menu
     toggle.classList.toggle("show-icon");
   });
 };
